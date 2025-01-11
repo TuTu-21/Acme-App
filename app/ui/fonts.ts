063163fafa2ce7490a74baabed5bf9dd5924e4a4
Aishik Chowdhury <110451960+TuTu-21@users.noreply.github.com>
2025-01-11 12:35:04 +0600

    Add files via upload

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Abel } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const abel = Abel( {subsets: ['latin'], // Specify subsets as needed
+    weight: '400', // Specify weights if applicable
+    display: 'swap',
+ }); // Optional: Specify font display);
